a0940710d0df37e92551cafdd40a0d927cd276be
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-07-04 23:42:54 +0530

    Add :Linked list creation.

---
+++ DataStructures/src/main/java/com/bridgelabz/DataStructures/LinkedListMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.DataStructures;
+
+/* create a simple Linked List
+* of 56, 30 and 70 */
+public class LinkedListMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Lets Create Linked List to add 56,30 & 70!");
+	}
+
+}
