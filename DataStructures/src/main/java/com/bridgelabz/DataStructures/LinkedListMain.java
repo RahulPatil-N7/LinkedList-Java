4c7b4e218de6835543eb387694ee3b49e010f6d7
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-07-07 23:48:46 +0530

    Refactored : Nodes added one by one.

--- DataStructures/src/main/java/com/bridgelabz/DataStructures/LinkedListMain.java
+++ DataStructures/src/main/java/com/bridgelabz/DataStructures/LinkedListMain.java
@@ -6,7 +6,18 @@ public class LinkedListMain {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Lets Create Linked List to add 56,30 & 70!");
+		System.out.println("Lets Create Linked List to add 56,30 & 70!\n");
+		
+		LinkedList list = new LinkedList();
+		Node<Integer> firstNode = new Node<Integer>(56);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(70);
+		
+		//Using list object of LinkeList to call Node operations
+		list.addNode(firstNode);
+		list.addNode(secondNode);
+		list.addNode(thirdNode);
+		list.display(firstNode);
 	}
 
 }
